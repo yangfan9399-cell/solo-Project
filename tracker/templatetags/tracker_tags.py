5be342a29c769d4b9d00460f13d0c06304187206
yang <yf_7715@163.com>
2026-06-02 17:05:31 +0800

    feat: unify inspection handling presentation

--- tracker/templatetags/tracker_tags.py
+++ tracker/templatetags/tracker_tags.py
@@ -8,3 +8,25 @@ def dict_get(d, key):
     if isinstance(d, dict):
         return d.get(key, 0)
     return 0
+
+
+@register.filter
+def empty_dash(value):
+    return value if value else '-'
+
+
+@register.filter
+def format_datetime(value):
+    if not value:
+        return '-'
+    return value.strftime('%Y-%m-%d %H:%M')
+
+
+@register.filter
+def truncate_middle(value, length):
+    if not value:
+        return '-'
+    if len(value) <= length:
+        return value
+    return value[:length] + '...'
+
