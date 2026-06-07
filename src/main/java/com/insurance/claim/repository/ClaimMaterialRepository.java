d39d48d965efd4f9a7c75a09bcd452f32e5c9148
yang <yf_7715@163.com>
2026-06-07 11:31:55 +0800

    chore(q-068): round 3 Trae output

--- src/main/java/com/insurance/claim/repository/ClaimMaterialRepository.java
+++ src/main/java/com/insurance/claim/repository/ClaimMaterialRepository.java
@@ -3,6 +3,7 @@ package com.insurance.claim.repository;
 import com.insurance.claim.entity.ClaimMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface ClaimMaterialRepository extends JpaRepository<ClaimMaterial, Long> {
     List<ClaimMaterial> findByClaimCaseIdOrderByUploadTimeDesc(Long claimCaseId);
