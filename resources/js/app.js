5d50e4dd1f8fc73af9e6cd8d80c2e8d22b5a9087
yang <yf_7715@163.com>
2026-06-10 16:55:56 +0800

    chore(q-118): round 2 Trae output

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,11 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import { resolvePageComponent } from 'laravel-vite'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
 
 createInertiaApp({
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  resolve: (name) => import(`./Pages/${name}.vue`),
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
       .use(plugin)
-      .use(ZiggyVue)
       .mount(el)
   },
 })
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商户装修审批系统</title>
+    @vite('resources/css/app.css')
+    @inertiaHead
+</head>
+<body>
+    @inertia
+    @vite('resources/js/app.js')
+</body>
+</html>
